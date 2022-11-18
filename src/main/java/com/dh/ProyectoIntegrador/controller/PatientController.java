8904b1845c6ed5021376331ee1d46d370e522ca4
morbdeath <nahuel_barbosa1990@hotmail.com.ar>
2022-11-17 23:00:48 -0300

    Se agregaron comentarios para guiar al funcionamiento del codigo al que lo vea

--- src/main/java/com/dh/ProyectoIntegrador/controller/PatientController.java
+++ src/main/java/com/dh/ProyectoIntegrador/controller/PatientController.java
@@ -5,20 +5,27 @@ import com.dh.ProyectoIntegrador.repository.impl.PatientRepository;
 import com.dh.ProyectoIntegrador.service.PatientService;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/patient")
+@RestController//Lo ponemos al que se comunica con la vista o sea el front
+@RequestMapping("/patient")//Aca designo como va a ser la url
 public class PatientController {
     static Logger log = Logger.getLogger(PatientController.class);
 
     //@Autowired(required = true)
     //private Log log = new Log();
+
     private PatientService patientService = new PatientService(new PatientRepository());
+
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Patient> getId(@PathVariable int id) {
@@ -26,18 +33,24 @@ public class PatientController {
         return ResponseEntity.ok(patientService.getId(id));
     }
 
+    //Obtenemos todos los pacientes
+    //http://localhost:8080/patient
     @GetMapping()
     public List<Patient> getAll() {
         log.info("Se solicitan todos los pacientes");
         return patientService.getAll();
     }
 
+    //Guardamos los pacientes y de tener el mismo id lo modificamos
+    //http://localhost:8080/patient/save    (en el postman cargamos los datos en el body)
     @PostMapping("/save")
     public Patient save(@RequestBody Patient patient) {
         log.info("Se guardo el paciente");
         return patientService.save(patient);
     }
 
+    //Eliminamos un paciente
+    //http://localhost:8080/patient?id=xx  ingresamos el id del que queremos eliminar
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable int id) {
         log.info("Se elimino el paciente con el ID:" + id);
--- src/main/java/com/dh/ProyectoIntegrador/entity/Patient.java
+++ src/main/java/com/dh/ProyectoIntegrador/entity/Patient.java
@@ -3,17 +3,18 @@ package com.dh.ProyectoIntegrador.entity;
 import lombok.*;
 import javax.persistence.*;
 
-@Data
+
 @AllArgsConstructor
-@NoArgsConstructor
-@Table(name="patients")
-@Entity
+@Data//Me crea todos los getters y setters de las variables
+@Entity// Para la base de datos con ORM
+@Table(name="patients")//Va a crear una tabla en la base de datos con el nombre patients si no lo tiene
+@NoArgsConstructor//Crea un constructor sin parametros
 public class Patient {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Para ponerla como id principal
     private int Id;
-    @Column(nullable = false)
+    @Column(nullable = false)//No admite nulos con false con true si, cuando se tenga que ingresar un dato
     private String SName;
     @Column(nullable = false)
     private String SLastname;
@@ -23,5 +24,4 @@ public class Patient {
     private String SDni;
     @Column(nullable = false)
     private String SHighdate;
-
 }
